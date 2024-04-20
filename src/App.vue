64282354da01d0ba280450fb460ad42bc71ff39c
RayChen <ray.10315332@gmail.com>
2024-04-21 00:15:42 +0800

    發文列表區塊

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
-<script setup lang="ts"></script>
+<script setup>
+import Header from "@/components/Header.vue";
+import PostCell from "@/components/PostCell/cell.vue";
+import SearchBar from "@/components/searchInput.vue";
+import CustomSelect from "@/components/CategorySelect.vue";
+</script>
 
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
-  </div>
+  <main class="main bg-repeat bg-center">
+    <Header />
+    <div class="min-h-screen h-full px-4 max-w-[1440px] mx-auto">
+      <div class="h-20 mt-12 flex gap-3">
+        <CustomSelect />
+        <SearchBar />
+      </div>
+      <PostCell />
+    </div>
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.main {
+  background-image: url("./assets/img/bg.svg");
+}
+</style>
---
+++ src/components/CategorySelect.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="h-full">
+    <select class="border-2 p-2 w-36 bg-white">
+      <option>最新貼文</option>
+    </select>
+  </div>
+</template>
