122058423579736462a73a78238ef522f8e435d8
ireneshih <90667518+ireneshih@users.noreply.github.com>
2024-04-22 22:38:11 +0800

    add PageTitle component

---
+++ src/components/PageTitle.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="wrapper w-[500px] text-center">
+    <div
+      class="bg-white p-4 border-solid border-2 border-black text-2xl font-medium"
+    >
+      張貼動態
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  position: relative;
+  z-index: 1;
+}
+
+.wrapper::after {
+  display: block;
+  content: "";
+  background: #fff;
+  border: 2px solid #000400;
+  height: 72px;
+  width: 100%;
+  position: absolute;
+  right: 5px;
+  bottom: -5px;
+  z-index: -1;
+}
+</style>
